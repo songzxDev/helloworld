6710c668ad59dd7ede7037459c4e6a33b7589102
songzxDev <songzx_2326@163.com>
2017-10-24 14:36:20 +0800

    调整切面类切入表达式

--- src/cn/songzx/helloworld/workflow/aspect/WorkflowDataSourceAspect.java
+++ src/cn/songzx/helloworld/workflow/aspect/WorkflowDataSourceAspect.java
@@ -22,25 +22,25 @@ import cn.songzx.helloworld.workflow.dao.holder.DataSourceContextHolder;
 @Component
 public class WorkflowDataSourceAspect {
 
-	@Before("execution(* cn.songzx.helloworld.workflow.biz.*.*(..))")
+	@Before("execution(* cn.songzx.helloworld.workflow.biz..*.*(..))")
 	public void setBpmDataSource(JoinPoint jp) {
 		System.out.println("当前线程【" + Thread.currentThread().getName() + "】执行了：setCustomerType(String value)方法，动态指定了数据源！");
 		DataSourceContextHolder.setCustomerType(DynamicDataSourceLookupKey.BPM_DATASOURCE.getLookupKey());
 	}
 
-	@After("execution(* cn.songzx.helloworld.workflow.biz.*.*(..))")
+	@After("execution(* cn.songzx.helloworld.workflow.biz..*.*(..))")
 	public void removeBpmDataSource(JoinPoint jp) {
 		System.out.println("当前线程【" + Thread.currentThread().getName() + "】执行了：clearCustomerType()方法，清除了数据源！");
 		DataSourceContextHolder.clearCustomerType();
 	}
 
-	@Before("execution(* cn.songzx.helloworld.oabiz.wf.service.*.*(..))")
+	@Before("execution(* cn.songzx.helloworld.oabiz.wf.service..*.*(..))")
 	public void setBizWFDataSource(JoinPoint jp) {
 		System.out.println("当前线程【" + Thread.currentThread().getName() + "】执行了：setCustomerType(String value)方法，动态指定了数据源！");
 		DataSourceContextHolder.setCustomerType(DynamicDataSourceLookupKey.BIZ_DATASOURCE.getLookupKey());
 	}
 
-	@After("execution(* cn.songzx.helloworld.oabiz.wf.service.*.*(..))")
+	@After("execution(* cn.songzx.helloworld.oabiz.wf.service..*.*(..))")
 	public void removeBizWFDataSource(JoinPoint jp) {
 		System.out.println("当前线程【" + Thread.currentThread().getName() + "】执行了：clearCustomerType()方法，清除了数据源！");
 		DataSourceContextHolder.clearCustomerType();
