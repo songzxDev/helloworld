26798e25079f21b09c75a4df5150cc752efe3b7b
songzxDev <songzx_2326@163.com>
2017-11-03 16:52:12 +0800

    新增了流程建模变量枚举

---
+++ src/cn/songzx/helloworld/oabiz/wf/enmu/WFVariableType.java
@@ -0,0 +1,34 @@
+/**
+* @Title: WFVariableType.java
+* @Package cn.songzx.helloworld.oabiz.wf.enmu
+* @Description: TODO(用一句话描述该文件做什么)
+* @author Songzx songzx_2326@163.com
+* @date 2017年11月3日 下午4:31:36
+* @version V1.0
+*/
+package cn.songzx.helloworld.oabiz.wf.enmu;
+
+/**
+ * @ClassName: WFVariableType
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author Songzx songzx_2326@163.com
+ * @date 2017年11月3日 下午4:31:36
+ *
+ */
+public enum WFVariableType {
+	current_participant_name,
+	current_participant_partyid,
+	current_participant_code,
+	current_participant_dept_name,
+	current_participant_dept_code,
+	previous_participant_name,
+	previous_participant_partyid,
+	previous_participant_code,
+	previous_participant_dept_name,
+	previous_participant_dept_code,
+	business_bill_id,
+	business_bill_name,
+	business_bill_no,
+	business_bill_kind_id,
+	business_bill_kind_name;
+}
